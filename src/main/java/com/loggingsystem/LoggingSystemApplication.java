86794a4fbe53d58040d15a7bb636f9f9928e9f5b
Fejin Francis <fejinfm2000@gmail.com>
2025-06-23 19:58:40 +0530

    initial commit

---
+++ src/main/java/com/loggingsystem/LoggingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.loggingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggingSystemApplication.class, args);
+	}
+
+}
